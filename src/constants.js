691ef07eaf058918cdf39466bb0f2674627819ce
phillip <kip.phil@gmail.com>
2019-12-02 17:40:34 -0800

    Add code to interface with TF

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const WORD_INDEX = {'the': 1, 'shape': 2, 'is': 3, 'a': 4, 'image': 5, 'what': 6, 'in': 7, 'does': 8, 'contain': 9, 'present': 10, 'there': 11, 'not': 12, 'color': 13, 'no': 14, 'triangle': 15, 'circle': 16, 'rectangle': 17, 'of': 18, 'green': 19, 'brown': 20, 'teal': 21, 'gray': 22, 'blue': 23, 'black': 24, 'yellow': 25, 'red': 26};
+const ANSWERS = ['teal', 'yellow', 'triangle', 'no', 'yes', 'brown', 'circle', 'blue', 'black', 'red', 'gray', 'green', 'rectangle'];
+
+export {WORD_INDEX, ANSWERS};
