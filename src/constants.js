9d427aa050aa8b5993fe4d6a37368af7606f705a
Victor Zhou <vzhou842@gmail.com>
2019-12-27 10:47:09 +1000

    Upload new model trained on 8k images / 90k questions, 99%+ val acc

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 // All of these constants should be kept in sync with their counterparts in easy-VQA,
 // the repo that contains the code to generate the dataset.
 
-const WORD_INDEX = {'the': 1, 'shape': 2, 'is': 3, 'a': 4, 'image': 5, 'what': 6, 'does': 7, 'contain': 8, 'in': 9, 'present': 10, 'not': 11, 'there': 12, 'color': 13, 'no': 14, 'triangle': 15, 'circle': 16, 'rectangle': 17, 'of': 18, 'red': 19, 'yellow': 20, 'teal': 21, 'black': 22, 'green': 23, 'blue': 24, 'brown': 25, 'gray': 26};
-const ANSWERS = ['rectangle', 'brown', 'red', 'black', 'blue', 'green', 'gray', 'no', 'teal', 'triangle', 'yes', 'circle', 'yellow'];
+const WORD_INDEX = {'is': 1, 'shape': 2, 'the': 3, 'a': 4, 'image': 5, 'there': 6, 'not': 7, 'what': 8, 'in': 9, 'does': 10, 'contain': 11, 'present': 12, 'color': 13, 'no': 14, 'triangle': 15, 'circle': 16, 'rectangle': 17, 'green': 18, 'gray': 19, 'blue': 20, 'black': 21, 'brown': 22, 'teal': 23, 'yellow': 24, 'red': 25, 'of': 26};
+const ANSWERS = ['no', 'rectangle', 'circle', 'yes', 'teal', 'blue', 'green', 'yellow', 'gray', 'black', 'brown', 'red', 'triangle'];
 
 const IMAGE_SIZE = 64;
 const MIN_SHAPE_SIZE = IMAGE_SIZE / 8;
